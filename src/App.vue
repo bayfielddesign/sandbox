f770684964a63f5d8c0143c1543d1d74073592f5
r-hague <r.m.hague@gmail.com>
2020-12-28 20:45:51 -0500

    submission by Rachel Hague

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,74 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" role="main">
+    <h1>User Registration Form</h1>
+    <user-reg-form @user-added="addUser"></user-reg-form>
+    <h2>Registered Users</h2>
+    <ul>
+      <li v-for="user in Users" :key="user.id">
+        <user
+          :fname="user.fname"
+          :lname="user.lname"
+          :eAddress="user.eAddress"
+          :id="user.id"
+        ></user>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import User from "./components/User.vue";
+import uniqueId from "lodash.uniqueid";
+import UserRegForm from "./components/UserRegForm";
 
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
-  }
-}
+    UserRegForm,
+    User,
+  },
+  data() {
+    return {
+      Users: [
+        {
+          id: uniqueId("user-"),
+          fname: "Clayton",
+          lname: "Lamb",
+          eAddress: "clamb@org.org",
+        },
+        {
+          id: uniqueId("user-"),
+          fname: "Madeline",
+          lname: "Johnson",
+          eAddress: "maddi@live.com",
+        },
+        {
+          id: uniqueId("user-"),
+          fname: "Wayne",
+          lname: "Moody",
+          eAddress: "wayne@moody.com",
+        },
+        {
+          id: uniqueId("user-"),
+          fname: "Lonnie",
+          lname: "Hayes",
+          eAddress: "lonnie.hayes@company.com",
+        },
+      ],
+    };
+  },
+  methods: {
+    addUser(fname, lname, eAddress, pwd) {
+      this.Users.push({
+        id: uniqueId("user-"),
+        fname: fname,
+        lname: lname,
+        eAddress: eAddress,
+        pwd: pwd,
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss">
@@ -24,5 +79,56 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  border-radius: 30px;
+  background-color: #f2f2f2;
+  padding: 20px;
 }
+h2 {
+  text-align: left;
+}
+li {
+  text-align: left;
+  font-size: 20px;
+}
+
+input {
+  width: 45%;
+  height: 30px;
+  border-radius: 5px;
+  background-color: lightblue;
+  margin-left: 2px;
+  position: relative;
+}
+
+input:focus {
+  background-color: white;
+}
+
+.form-label {
+  display: inline-block;
+  position: relative;
+  width: 60%;
+  margin: 20px 0px 0px;
+  padding: 5px;
+  font-size: 20px;
+}
+
+button {
+  width: 20%;
+  background-color:lightblue;
+  font-size: 20px;
+  padding: 14px 20px;
+  margin: 8px;
+  //border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+button:hover {
+  //background-color: blue;
+  font-weight: bold;
+  border: 2px solid black;
+}
+
+
 </style>
---
+++ src/components/UserRegForm.vue
@@ -0,0 +1,92 @@
+<template>
+  <form>
+    <label for="fname" class="form-label"> First Name: </label>
+    <input
+      type="text"
+      id="fname"
+      name="fname"
+      autocomplete="off"
+      v-model.lazy.trim="fname"
+    /><br />
+    <label for="lname" class="form-label"> Last Name: </label>
+    <input
+      type="text"
+      id="lname"
+      name="lname"
+      autocomplete="off"
+      v-model.lazy.trim="lname"
+    /><br />
+    <label for="eAddress" class="form-label"> Email Address: </label>
+    <input
+      type="email"
+      id="eAddress"
+      name="eAddress"
+      autocomplete="off"
+      v-model.lazy.trim="eAddress"
+    /><br />
+    <label for="pwd" class="form-label"> Password: </label>
+    <input
+      type="password"
+      id="pwd"
+      name="pwd"
+      autocomplete="off"
+      v-model.lazy.trim="pwd"
+    /><br />
+    <label for="pwd2" class="form-label"> Confirm Password: </label>
+    <input
+      type="password"
+      id="pwd2"
+      name="pwd2"
+      autocomplete="off"
+      v-model.lazy.trim="pwd2"
+    /><br />
+    <button v-on:click.prevent="clear">Clear</button>
+    <button v-on:click.prevent="onSubmit">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  methods: {
+    onSubmit() {
+      if (this.fname === "") {
+        alert("First name is required");
+        return;
+      } else if (this.lname === "") {
+        alert("Last name is required");
+        return;
+      } else if (this.eAddress.includes("@") == false || this.eAddress === "") {
+        alert("Email address is required and must include the @ symbol");
+        return;
+      } else if (this.pwd === "" || this.pwd !== this.pwd2) {
+        alert("Password is required and both passwords must match");
+        return;
+      }
+      this.$emit("user-added", this.fname, this.lname, this.eAddress, this.pwd);
+      alert("User added successfully");
+      this.fname = "";
+      this.lname = "";
+      this.eAddress = "";
+      this.pwd = "";
+      this.pwd2 = "";
+    },
+    clear() {
+      console.log("clear clicked");
+      this.fname = "";
+      this.lname = "";
+      this.eAddress = "";
+      this.pwd = "";
+      this.pwd2 = "";
+    },
+  },
+  data() {
+    return {
+      fname: "",
+      lname: "",
+      eAddress: "",
+      pwd: "",
+      pwd2: "",
+    };
+  },
+};
+</script>
