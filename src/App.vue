51e7c7df5cd94e1c8ba23c16be1fbca90cb8efa9
Lroes <lucas.g.roes@icloud.com>
2020-12-29 20:39:33 -0500

    Created Registration Form component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
+<!--
+ * @file   App.vue: Entry point for the vue components
+ * @author Lucas Roes.
+ * Created: December 27, 2020.
+ -->
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <img width="300" height="117" alt="Bayfield Design logo" src="./assets/BD_logo.png">
+      <hr>
+    </div>
+    <main>
+      <RegForm/>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RegForm from './components/RegForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RegForm
   }
 }
 </script>
 
 <style lang="scss">
+
+$main-color: #225773;
+$accent-color: #bf5543;
+$bg-color: #eaeaea;
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: $main-color;
+  margin-top: 30px;
+}
+
+img {
+  height: 100%;
+  aspect-ratio: attr(width) / attr(height);
+}
+
+hr {
+  border: 1px solid $accent-color;
+  margin-top: 30px;
+  margin-bottom: 30px;
 }
 </style>
